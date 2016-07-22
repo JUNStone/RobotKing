367d7bcd566e3b1e5ccc8e49b7e177a3ea88feb8
JUNStone <j00mst0m3@gmail.com>
2016-07-22 21:14:37 +0900

    Object Pool
    오브젝트 풀 클래스를 작성하였읍니다

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPool
+{
+	int objectId = 1;
+
+	Stack<GameObject> inactive;
+
+	GameObject prefab;
+
+	public ObjectPool(GameObject prefab, int size)
+	{
+		this.prefab = prefab;
+		inactive = new Stack<GameObject>(size);
+	}
+
+	public GameObject Spawn(Vector3 pos, Quaternion rot = Quaternion.identity)
+	{
+		GameObject obj;
+	
+		if(inactive.Count == 0) {
+			obj = (GameObject)GameObject.Instantiate(prefab, pos, rot);
+			obj.name = prefab.name + " ("+(objectId++)+")";
+		} else {
+			obj = inactive.Pop();
+			if(obj == null) { return Spawn(pos); }
+		}
+
+		obj.transform.position = pos;
+		obj.transform.rotation = rot;
+		obj.SetActive(true);
+		return obj;
+	}
+
+	public void Remove(GameObject obj)
+	{
+		obj.SetActive(false);
+		inactive.Push(obj);
+	}
+
+	public void Preload(GameObject prefab, int size = 1)
+	{
+		GameObject[] obs = new GameObject[size];
+		for (int i = 0; i < size; i++) {
+			obs[i] = Spawn (prefab, Vector3.zero);
+		}
+	
+		for (int i = 0; i < size; i++) {
+			Remove(obs[i]);
+		}
+	}
+}
