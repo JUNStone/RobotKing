582ec3b9e11375f978d9d4f9e75aabcc8cddccc7
JUNStone <j00mst0m3@gmail.com>
2016-07-23 07:51:38 +0900

    이펙트 추가
    Weapon Change시에 Effect발생

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -25,10 +25,11 @@ public class PlayerScript : CharacterScript
 	int maxHuman;
 	int currentHuman;
 
-
 	GameObject rifle;
 	GameObject laser;
 	GameObject cannon;
+
+	GameObject weaponChangeEffect;
 
 	void Awake ()
 	{
@@ -38,14 +39,21 @@ public class PlayerScript : CharacterScript
 		laser = GameObject.Find ("Laser");
 		cannon = GameObject.Find ("Cannon");
 
+		weaponChangeEffect = GameObject.FindGameObjectWithTag ("WCEffect");
+		weaponChangeEffect.SetActive (false);
+
 		currentHuman = maxHuman;
 
 		SetStatus (CharacterStatus.Run);
 		SetWeapon (PlayerWeapon.rifle);
+
 	}
 
 	public void SetWeapon(PlayerWeapon status)
 	{
+		if (status != currentWeapon) {
+			StartCoroutine ("Effect");
+		}
 		currentWeapon = status;
 		switch (currentWeapon) {
 		case PlayerWeapon.rifle:
@@ -74,5 +82,12 @@ public class PlayerScript : CharacterScript
 		} else if (currentHuman < 0) {
 			SetStatus (CharacterStatus.Die);
 		}
+	}
+
+	IEnumerator Effect()
+	{
+		weaponChangeEffect.SetActive (true);
+		yield return new WaitForSeconds(0.5f);
+		weaponChangeEffect.SetActive (false);
 	}
 }
