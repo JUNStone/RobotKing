8005ab8e686b11a591d19346c256a5c8238427a6
nonami <noname129@naver.com>
2016-07-22 16:30:10 +0900

    MainScene 틀 완성

---
+++ Assets/Scripts/Dohyung's Scripts/MainSceneScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	
+	public void Btn_Start() {
+		// Application.LoadLevel("StartScene");
+		// move to start scene
+	}
+
+	public void Btn_Help() {
+		// popop
+	}
+}
