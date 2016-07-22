fc7b78d100eb693dcda10804510a4722f3322702
nonami <noname129@naver.com>
2016-07-22 21:25:16 +0900

    BulletCommon 일부 주석 처리

--- Assets/Scripts/BulletCommon.cs
+++ Assets/Scripts/BulletCommon.cs
@@ -20,7 +20,7 @@ public class BulletCommon : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.gameObject.transform.Translate (Vector3.right * speed * Time.smoothDeltaTime);
-		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		/*GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
 		for (int i = 0, len = enemies.Length; i < len; ++i) {
 			//when enemy collides
 			if (false && !hitEnemies.Contains (enemies [i])) {
@@ -32,15 +32,17 @@ public class BulletCommon : MonoBehaviour {
 					hitEnemies.Add (enemies [i]);
 				}
 			}
-		}
+		}*/
 
-		if (this.transform.position.x >= 2000) {
-			Inactivate ();
-		}
+		//if (this.transform.position.x >= 2000) {
+		//	Inactivate ();
+		//}
 	}
 
-	public void Inactivate() {
-		hitEnemies.Clear ();
-		this.gameObject.SetActive (false);
-	}
+
+
+	//public void Inactivate() {
+	//	hitEnemies.Clear ();
+	//	this.gameObject.SetActive (false);
+	//}
 }
