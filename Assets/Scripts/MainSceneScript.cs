81c91b53692e7f07f82746a90f7a28cf931a3488
nonami <noname129@naver.com>
2016-07-22 20:49:30 +0900

    MainScene 게임방법 팝업 구현 및 스크립트 디렉터리 정리

--- Assets/Scripts/MainSceneScript.cs
+++ Assets/Scripts/MainSceneScript.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class MainSceneScript : MonoBehaviour {
 
 	// Use this for initialization
-	
+	GameObject popup;
+
+	void Awake() {
+		popup = GameObject.Find ("Howto Popup");
+		popup.SetActive (false);
+	}
+
 	public void Btn_Start() {
 		// Application.LoadLevel("StartScene");
 		// move to start scene
 	}
 
 	public void Btn_Help() {
-		// popop
+		popup.SetActive (true);
+	}
+
+	public void Btn_DismissHelp() {
+		popup.SetActive (false);
 	}
 }
