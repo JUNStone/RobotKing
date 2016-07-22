e27e596a41cc3419cd28257ef0b8b07de771388e
nonami <noname129@naver.com>
2016-07-23 02:43:49 +0900

    일시정지, 게임오버 팝업 추가[이준석씹새끼 자살각]

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScript : MonoBehaviour {
+
+	GameObject gameOverPopup;
+
+	void Awake () {
+		gameOverPopup = GameObject.Find ("GameOverPopup");
+		gameOverPopup.SetActive (false);
+	}
+
+	public void RestartButtonClick() {
+		Debug.Log ("Restart");
+		Application.LoadLevel (0);
+	}
+
+	public void GoToMenuButtonClick() {
+	}
+}
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+	[SerializeField]
+	GameObject pausePopup;
+	GameObject pauseButton;
+
+	void Awake() {
+		pausePopup = GameObject.Find ("PausePopup");
+		pausePopup.SetActive (false);
+
+		pauseButton = GameObject.Find ("PauseButton");
+	}
+
+	public void PauseButtonClick() {
+		pausePopup.SetActive (true);
+		pauseButton.SetActive (false);
+		Time.timeScale = 0.0f;
+	}
+
+	public void ResumeButtonClick() {
+		pausePopup.SetActive (false);
+		pauseButton.SetActive (true);
+		Time.timeScale = 1.0f;
+	}
+
+	public void MenuButtonClick() {
+	}
+}
