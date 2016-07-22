3132532e166e177280b120230c9829233626990c
JUNStone <j00mst0m3@gmail.com>
2016-07-22 17:37:25 +0900

    Add Background Scrolling
    Yeah

---
+++ Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScript : MonoBehaviour {
+
+	[SerializeField]
+	GameObject[] sprites;
+	[SerializeField]
+	Vector3[] startPosition;
+
+	// 임시 변수
+	public float speed;
+	public float imageOffsetX;
+
+	void Awake ()
+	{
+		sprites = GameObject.FindGameObjectsWithTag ("Background");
+		startPosition = new Vector3[sprites.Length];
+
+		for (int i = sprites.Length-1; i >= 0; --i) {
+			startPosition [i] = sprites [i].transform.localPosition;
+		}
+	}
+
+	void Update ()
+	{
+		for (int i = sprites.Length - 1; i >= 0; --i) {
+			sprites [i].transform.Translate (Vector3.left * Time.smoothDeltaTime * speed);
+			if (sprites [i].transform.localPosition.x < imageOffsetX * -1) { sprites [i].transform.localPosition = new Vector3 (imageOffsetX * (sprites.Length - 1), 0.0f, 0.0f); }
+		}
+	}
+}
